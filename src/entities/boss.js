b90e8756f7aa8fbb7cee1d26c501223791fa6446
c.klewar <c.klewar@f5.com>
2025-01-17 19:58:44 +0100

    add boss anim

--- src/entities/boss.js
+++ src/entities/boss.js
@@ -2,8 +2,8 @@ import { bossState } from "../state/bossState.js";
 import * as consts from "../const.js";
 
 const boss_attrs = new Map([
-  ["boss_1", { vec_x: 15, vec_y: 55, rect_x: 220, rect_y: 120, png: "daemon_6", body: false}],
-  ["boss_2", { vec_x: 0, vec_y: -5, rect_x: 128, rect_y: 130, png: "daemon_4", body: true}],
+  ["boss_1", { vec_x: 15, vec_y: 55, rect_x: 220, rect_y: 120, png: "daemon_6", body: false, scale: 2, move: false}],
+  ["boss_2", { vec_x: 0, vec_y: -5, rect_x: 128, rect_y: 130, png: "daemon_4", body: false, scale: 2, move: true}],
 ]);
 
 export function generateBossComponents(k, boss_type, pos, level, player) {
@@ -11,7 +11,7 @@ export function generateBossComponents(k, boss_type, pos, level, player) {
     var items = [
 			k.sprite(attr.png, { anim: "idle" }),
 			k.area({ shape: new Rect(vec2(attr.vec_x, attr.vec_y), attr.rect_x, attr.rect_y) }),
-			k.scale(2),
+			k.scale(attr.scale),
 			k.pos(pos),
 			k.anchor("center"),
 			k.state("move", ["idle", "attack", "move"]),
@@ -21,7 +21,7 @@ export function generateBossComponents(k, boss_type, pos, level, player) {
               attackPower: 1.0,
               direction: "left",
               isAttacking: false,
-              weapon: "axe",
+              weapon: "spike",
               entityState: bossState,
             },
             "boss"
@@ -30,49 +30,59 @@ export function generateBossComponents(k, boss_type, pos, level, player) {
     if (attr.body) items.push(k.body())
     const boss = level.spawn(items, 2, 2);
 
-    /*boss.onStateEnter("idle", async () => {
-        await wait(0.5);
-        boss.enterState("attack");
-    });
+    if (attr.move) {
+        boss.onStateEnter("idle", async () => {
+            await wait(0.5);
+            boss.enterState("attack");
+        });
 
-    boss.onStateEnter("attack", async () => {
-        if (!boss.exists()) {
-            return;
-        }
+        boss.onStateEnter("attack", async () => {
+            if (!boss.exists()) {
+                return;
+            }
 
-		if (player.exists()) {
-			const dir = player.pos.sub(boss.pos).unit();
+            var deg = 0;
+            if (player.exists()) {
+                const dir = player.pos.sub(boss.pos).unit();
 
-			k.add([
-				k.sprite("axe_1", { anim: "throw" }),
-				k.pos(boss.pos),
-				k.area({ shape: new Rect(vec2(0, 16), 24, 24) }),
-				k.move(dir, 300),
-				k.scale(3),
-				k.area(),
-				k.offscreen({ destroy: true }),
-				k.anchor("center"),
-				boss.weapon,
-				"boss",
-			]);
-		}
+                if (player.pos.x > boss.pos.x) {
+                    deg = Math.floor(Math.atan2(player.pos.y, player.pos.x) * (-180 / Math.PI));
+                } else {
+                    deg = Math.floor(Math.atan2(player.pos.y, player.pos.x) * (90 / Math.PI));
+                }
 
-		// Waits 1 second to make the boss enter in "move" state
-		await wait(1);
-		boss.enterState("move");
-	});
+                k.add([
+                    //k.sprite("spike_1", { anim: "throw" }),
+                    k.sprite("spike_1", {}),
+                    k.pos(boss.pos),
+                    k.area({ shape: new Rect(vec2(0, 16), 24, 24) }),
+                    k.move(dir, 900),
+                    k.rotate(deg),
+                    k.scale(3),
+                    k.area(),
+                    k.offscreen({ destroy: true }),
+                    k.anchor("center"),
+                    boss.weapon,
+                    "boss",
+                ]);
+            }
 
-	boss.onStateEnter("move", async () => {
-		await wait(2);
-		boss.enterState("idle");
-	});
+            // Waits 1 second to make the boss enter in "move" state
+            await wait(1);
+            boss.enterState("move");
+	    });
 
-	boss.onStateUpdate("move", () => {
-		// We move the boss in the direction of the player
-		if (!player.exists()) return;
-		const dir = player.pos.sub(boss.pos).unit();
-		boss.move(dir.scale(consts.ENEMY_SPEED));
-	});*/
+	    boss.onStateEnter("move", async () => {
+		    await wait(2);
+		    boss.enterState("idle");
+	    });
 
+	    boss.onStateUpdate("move", () => {
+            // We move the boss in the direction of the player
+            if (!player.exists()) return;
+            const dir = player.pos.sub(boss.pos).unit();
+            boss.move(dir.scale(consts.ENEMY_SPEED));
+	    });
+    }
   return boss;
 }
