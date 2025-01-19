13a36b8dee86232c23c97dc2acc4f10ce3857de9
Liam Klewar <lklewar@t-online.de>
2025-01-19 20:28:12 +0100

    update

--- src/scenes/graveyard.js
+++ src/scenes/graveyard.js
@@ -67,7 +67,7 @@ export default async function graveyard(k, levelIdx) {
 		tileHeight: 64,
 		tiles: {
 			"=": () => [
-				sprite("grass", {}),
+				sprite("deadgrass_1", {}),
 				pos(0, height() - consts.LEVEL_HEIGHT_OFFSET),
 				area(),
 				body({ isStatic: true }),
