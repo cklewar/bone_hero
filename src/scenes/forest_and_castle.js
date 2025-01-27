84dbded7268d5fef9c921216c49c80ee709980f1
c.klewar <c.klewar@f5.com>
2025-01-27 19:38:40 +0100

    add player direction

--- src/scenes/forest_and_castle.js
+++ src/scenes/forest_and_castle.js
@@ -109,19 +109,30 @@ export default async function forest_and_castle(k, levelIdx) {
 	setPlayerControls(k, entities.player);
 	watchEntityHealth(k, entities.player);
 
-    //Enemy
-	entities.echse1 = generateEnemyComponents(k, "echse", k.vec2(width() / 2, height() - consts.PLAYER_START_POS_Y_OFFSET), level, entities.player, "echse1");
-	entities.echse2 = generateEnemyComponents(k, "echse", k.vec2(width() / 2 - 100 , height() - 525), level, entities.player, "echse2");
-    entities.echse3 = generateEnemyComponents(k, "echse", k.vec2(width() / 2 - 500, height() - 460), level, entities.player, "echse3");
-    watchEntityHealth(k, entities.echse1, entities);
-    watchEntityHealth(k, entities.echse2, entities);
-    watchEntityHealth(k, entities.echse3, entities);
 
-    //Collide
-    onCollideWithEnemy(k, entities.player, entities.player.entityState, entities.echse1);
-    onCollideWithEnemy(k, entities.player, entities.player.entityState, entities.echse2);
-    onCollideWithEnemy(k, entities.player, entities.player.entityState, entities.echse3);
-    onCollideWithEnemy(k, entities.echse1, entities.echse1.entityState, entities.player);
-    onCollideWithEnemy(k, entities.echse2, entities.echse2.entityState, entities.player);
-    onCollideWithEnemy(k, entities.echse3, entities.echse3.entityState, entities.player);
+    switch (levelIdx) {
+		case 0:
+			 //Enemy
+            entities.echse1 = generateEnemyComponents(k, "echse", k.vec2(width() / 2, height() - consts.PLAYER_START_POS_Y_OFFSET), level, entities.player, "echse1");
+            entities.echse2 = generateEnemyComponents(k, "echse", k.vec2(width() / 2 - 100 , height() - 525), level, entities.player, "echse2");
+            entities.echse3 = generateEnemyComponents(k, "echse", k.vec2(width() / 2 - 500, height() - 460), level, entities.player, "echse3");
+            watchEntityHealth(k, entities.echse1, entities);
+            watchEntityHealth(k, entities.echse2, entities);
+            watchEntityHealth(k, entities.echse3, entities);
+
+            //Collide
+            onCollideWithEnemy(k, entities.player, entities.player.entityState, entities.echse1);
+            onCollideWithEnemy(k, entities.player, entities.player.entityState, entities.echse2);
+            onCollideWithEnemy(k, entities.player, entities.player.entityState, entities.echse3);
+            onCollideWithEnemy(k, entities.echse1, entities.echse1.entityState, entities.player);
+            onCollideWithEnemy(k, entities.echse2, entities.echse2.entityState, entities.player);
+            onCollideWithEnemy(k, entities.echse3, entities.echse3.entityState, entities.player);
+		  	break;
+		case 1:
+			break;
+		case 2:
+		  break;
+		default:
+		  console.log(`Sorry, we are out of ${expr}.`);
+	}
 }
